7ba350359f40d19526b4b2994972d19dee1220b6
TrahoDoesStuff <trahodoesstuff@gmail.com>
2024-01-05 23:46:03 +0000

    added aerial.nvim

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    on_attach = function(bufnr)
+      -- Jump forwards/backwards with '{' and '}'
+      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+    end
+
+    vim.keymap.set("n", "<C-a>", "<cmd>AerialToggle<CR>", {})
+
+    require("aerial").setup({})
+  end,
+}
