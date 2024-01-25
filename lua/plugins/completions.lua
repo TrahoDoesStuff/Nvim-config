b58b88d7423830f0d7f4eab0197c5517c50b5e9c
TrahoDoesStuff <Trahodoesstuff@gmail.com>
2024-01-25 18:04:22 +0000

    Java lsp config added

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,31 +1,24 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp",
-    lazy = false,
-    config = true,
+    "hrsh7th/cmp-nvim-lsp"
   },
   {
     "L3MON4D3/LuaSnip",
-    lazy = false,
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
     },
-
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
   },
   {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
@@ -37,7 +30,7 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
