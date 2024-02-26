1206cd063d6d9a77fd9e70075ba2d6fb562fd0c1
Renee Ibeji <reneeibeji@gmail.com>
2024-02-26 17:20:05 +0000

    Removed commands from live server as it wasn't working

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,6 +1,8 @@
 return {
-	"barrett-ruth/live-server.nvim",
-	build = "pnpm add -g live-server",
-	cmd = { "LiveServerStart", "LiveServerStop" },
-	config = true,
+  "barrett-ruth/live-server.nvim",
+  build = "pnpm add -g live-server",
+  cmd = { "LiveServerStart", "LiveServerStop" },
+  config = function()
+    require("live_server").setup()
+  end
 }
